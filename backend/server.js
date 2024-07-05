3872c4aee1e651316b1e4d85782b1208b1c8ce33
Nikhil <nikhilpal2017@gmail.com>
2024-07-05 12:26:38 +0530

    created listFood,addFood,and remove food API

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
+import foodRouter from './routes/foodRoute.js';
 
 
 // app config
 const app = express();
 const port = 3000
 
-
+//middleware configuration
 app.use(express.json());
 app.use(cors());
 
 //db connection
 connectDB();
+
+// api endpoints
+app.use("/api/food", foodRouter)
+app.use("/images",express.static('uploads'));
+
 
 app.get('/', (req, res) => {
     res.send("API Working")
